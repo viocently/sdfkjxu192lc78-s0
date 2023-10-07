a7dd8118182bf77784009e6f603cea521899aafc
viocently <73063414+viocently@users.noreply.github.com>
2023-10-07 14:23:11 +0800

    Delete code directory

--- code/exec.sh
+++
@@ -1,7 +0,0 @@
-mkdir LOG
-mkdir TERM
-mkdir STATE
-g++ deg.cpp SuperpolyBGL.cpp  -o mitm  -std=c++17 -O3 -lm -lpthread -I/$GUROBI_HOME/include/     -L/$GUROBI_HOME/lib -lgurobi_c++ -lgurobi95 -lm
-
-
-
